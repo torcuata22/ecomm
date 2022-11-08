866c6db38befcefe8d9094e72de8f19aab39b642
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-08 16:16:23 -0600

    finished playing with admin panel

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -8,11 +8,19 @@ admin.site.index_title = "ABC Shopping"
 
 #to display more infomration in the admin panel:
 class ProductAdmin(admin.ModelAdmin):
+    def changeCategoryToDefault(self,request, queryset):
+        queryset.update(category='default')
+
+    changeCategoryToDefault.short_description='Default Category'
     list_display=['title', 'price', 'discount_price', 'category']
-    search_fields=['title']
+    search_fields=['title', 'category']
+    actions=['changeCategoryToDefault',]
+    #fields = ['title', 'price',] this is how you cna hide certain fields
+    list_editable=['price', 'category'] #these fields are now editable in the list view
 
 class OrderAdmin(admin.ModelAdmin):
     list_display=["address", "name", "total"]
+    search_fields=["address", "name", "total"]
 
 admin.site.register(Products, ProductAdmin)
 admin.site.register(Order, OrderAdmin)
